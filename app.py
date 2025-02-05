8390fb222dd9d935492912fdcf2cd5fe9efb0762
Saad Abdur Razzaq <123338307+SaadARazzaq@users.noreply.github.com>
2025-02-05 11:03:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+GOOGLE_API_KEY = "AIzaSyDW61pwgBEvoDFXkjylIhRbDenqow5GHLw"
+
+genai.configure(api_key=GOOGLE_API_KEY)
 
 
 
